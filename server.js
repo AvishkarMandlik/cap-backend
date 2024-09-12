8ac625d0220fd1c7bb6ab7f5a7b8b20fedf29f37
AvishkarMandlik <mandlikavi121@gmail.com>
2024-09-12 11:54:34 +0530

    base build

--- server.js
+++ server.js
@@ -2,13 +2,22 @@ const express = require('express');
 const app = express();
 const bodyparser = require('body-parser');
 
+require("dotenv").config();
+const {connect ,disconnect} = require("./mongoConn.js");
+
 app.use(bodyparser.json());
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    
+
+app.get('/', async(req, res) => {
+    const db =  await connect();
+    const collection = db.collection('documents');
+
+    const insertResult = await collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }]);
+    console.log('Inserted documents =>', insertResult);
+    res.send(insertResult);
 });
 
 app.listen(3000, ()=>{
-    console.log('Avi Bhau Zinda Bad')
+    console.log('Sever listenning on Port 3000')
 });
